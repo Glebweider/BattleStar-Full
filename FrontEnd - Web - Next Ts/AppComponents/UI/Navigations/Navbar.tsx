ee7a42c12bb3aa10592882c12215cd90ae182282
Ku_r_umi <76429685+Glebweider@users.noreply.github.com>
2023-02-16 17:44:17 +0200

    Index.tsx update fixed navbar

--- FrontEnd - Web - Next Ts/AppComponents/UI/Navigations/Navbar.tsx
+++ FrontEnd - Web - Next Ts/AppComponents/UI/Navigations/Navbar.tsx
@@ -20,14 +20,14 @@ const Navbar:FC<PropsWithChildren> = ({children}) => {
   const [IsAuthUser, setIsAuthUser] = useState(false);
 
   const OpenMenuChat = () => {
-    document.querySelector('#buttonMenuChat')?.classList.toggle('Navbar_activeMenuButton__gpjH7');
-    document.querySelector('#buttonMenuNot')?.classList.remove('Navbar_activeMenuButton__gpjH7');
+    document.querySelector('#ButtonMenuChat')?.classList.toggle('Navbar_activeMenuButton__gpjH7');
+    document.querySelector('#ButtonMenuNot')?.classList.remove('Navbar_activeMenuButton__gpjH7');
     document.querySelector('#ChatMenu')?.classList.toggle('Navbar_activeMenu___PZfh');
     document.querySelector('#NotMenu')?.classList.add('Navbar_activeMenu___PZfh');
   }
   const OpenMenuNotoffication = () => {
-    document.querySelector('#buttonMenuChat')?.classList.remove('Navbar_activeMenuButton__gpjH7');
-    document.querySelector('#buttonMenuNot')?.classList.toggle('Navbar_activeMenuButton__gpjH7');
+    document.querySelector('#ButtonMenuChat')?.classList.remove('Navbar_activeMenuButton__gpjH7');
+    document.querySelector('#ButtonMenuNot')?.classList.toggle('Navbar_activeMenuButton__gpjH7');
     document.querySelector('#ChatMenu')?.classList.add('Navbar_activeMenu___PZfh');
     document.querySelector('#NotMenu')?.classList.toggle('Navbar_activeMenu___PZfh');
   }
@@ -57,8 +57,8 @@ const Navbar:FC<PropsWithChildren> = ({children}) => {
                 <div className={styles.Money}>
                   <div className={styles.MoneyBal}>
                     <a className={styles.BalanceText}>Баланс</a>                    
-                    <a href='/' className={styles.Plus}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="19.5" height="19.5" fill="#787878" viewBox="0 0 16 16">
+                    <a href='/' className={styles.ButtonAddMoney}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="19.5" height="19.5" fill="#606060" viewBox="0 0 16 16">
                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
                       </svg>
                     </a>
--- FrontEnd - Web - Next Ts/pages/index.tsx
+++ FrontEnd - Web - Next Ts/pages/index.tsx
@@ -1,12 +1,21 @@
-import styles from '@/styles/pages/Home.module.scss'
-import HeaderMeta from '@/AppComponents/Logic/Meta/Header.meta'
-import Navbar from '@/AppComponents/UI/Navigations/Navbar'
+import styles from '@/styles/pages/Home.module.scss';
+import HeaderMeta from '@/AppComponents/Logic/Meta/Header.meta';
+import Navbar from '@/AppComponents/UI/Navigations/Navbar';
+import Image from 'next/image';
+import MainWrapper from '@/public/Wrappers/WrapperMainPage.png'
 
 export default function Home() {
   return (
     <>
       <HeaderMeta title_description='Главная' description='' />
       <Navbar />
+      <main className={styles.Wrapper}>
+        <Image 
+          className={styles.WrapperImage}
+          src={MainWrapper} 
+          alt={'MainPage'} 
+        />
+      </main>
     </>
   )
 }
